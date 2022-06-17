6ee4497b093d23e90b2ae8db75a7b8b146086fe4
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-06-17 20:24:20 +0200

    add hardhat.config and .env example files

---
+++ hardhat.config.ts
@@ -0,0 +1,73 @@
+import { config as dotenvConfig } from 'dotenv';
+import { resolve } from 'path';
+import { HardhatUserConfig } from 'hardhat/types';
+import '@nomiclabs/hardhat-waffle';
+import '@nomiclabs/hardhat-ethers';
+import '@nomiclabs/hardhat-solhint';
+import 'hardhat-deploy';
+import 'hardhat-gas-reporter';
+
+dotenvConfig({ path: resolve(__dirname, './.env') });
+
+const CHAIN_IDS = {
+  HARDHAT: 1337,
+  MAINNET: 1,
+  POLYGON_MAINNET: 137,
+  MUMBAI_TESTNET: 80001
+};
+
+const MNEMONIC = process.env.MNEMONIC || '';
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || '';
+const INFURA_API_KEY = process.env.INFURA_API_KEY || '';
+const POLYGON_RPC = process.env.POLYGON_RPC || '';
+
+const getInfuraURL = (network: string) => {
+  return `https://${network}.infura.io/v3/${INFURA_API_KEY}`;
+};
+
+const config: HardhatUserConfig = {
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: {
+      chainId: CHAIN_IDS.HARDHAT,
+      accounts: { mnemonic: MNEMONIC }
+    },
+    mainnet: {
+      url: getInfuraURL('mainnet'),
+      chainId: CHAIN_IDS.MAINNET,
+      accounts: { mnemonic: MNEMONIC }
+    },
+    polygon: {
+      url: POLYGON_RPC,
+      chainId: CHAIN_IDS.POLYGON_MAINNET,
+      accounts: { mnemonic: MNEMONIC }
+    }
+  },
+  solidity: {
+    compilers: [
+      {
+        version: '0.8.6',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      }
+    ]
+  },
+  mocha: {
+    timeout: 8000000
+  },
+  gasReporter: {
+    currency: 'USD',
+    coinmarketcap: COINMARKETCAP_API_KEY
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0 // Here this will by default take the first account as deployer
+    }
+  }
+};
+
+export default config;
