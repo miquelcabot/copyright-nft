2000c8e488bfa15a09483a90b8f639c9fc987a2f
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-06-18 09:22:13 +0200

    add properties to ERC721

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -7,4 +7,23 @@ import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 contract ERC721 is Context {
     using SafeMath for uint256;
 
+    string private _name;
+    string private _symbol;
+    mapping(uint256 => address) private _owners;
+    mapping(address => uint256) private _balances;
+    mapping(uint256 => address) private _tokenApprovals;
+    mapping(address => mapping(address => bool)) private _operatorApprovals;
+
+    constructor(string memory name_, string memory symbol_) {
+        _name = name_;
+        _symbol = symbol_;
+    }
+
+    function name() external view returns (string memory) {
+        return _name;
+    }
+
+    function symbol() external view returns (string memory) {
+        return _symbol;
+    }
 }
