86739c61c7ef4215b8a8d3d5ee2247cf1d6a106f
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-06-18 10:33:40 +0200

    add baseURI and tokenURI

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 import "./interfaces/IERC721Metadata.sol";
 
 contract ERC721 is Context, IERC721Metadata {
@@ -10,6 +11,8 @@ contract ERC721 is Context, IERC721Metadata {
 
     string private _name;
     string private _symbol;
+    mapping(uint256 => string) private _tokenURIs;
+    string private _baseURI;
     mapping(uint256 => address) private _owners;
     mapping(address => uint256) private _balances;
     mapping(uint256 => address) private _tokenApprovals;
@@ -26,6 +29,36 @@ contract ERC721 is Context, IERC721Metadata {
 
     function symbol() external view override returns (string memory) {
         return _symbol;
+    }
+
+    function baseURI() public view returns (string memory) {
+        return _baseURI;
+    }
+
+    function tokenURI(uint256 tokenId)
+        external
+        view
+        override
+        returns (string memory)
+    {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: you can't query the URI for nonexistent token"
+        );
+
+        string memory _tokenURI = _tokenURIs[tokenId];
+        string memory base = baseURI();
+
+        // If there is no base URI, return the token URI.
+        if (bytes(base).length == 0) {
+            return _tokenURI;
+        }
+        // If both are set, concatenate the baseURI and tokenURI (via abi.encodePacked).
+        if (bytes(_tokenURI).length > 0) {
+            return string(abi.encodePacked(base, _tokenURI));
+        }
+        // If there is a baseURI but no tokenURI, concatenate the tokenID to the baseURI.
+        return string(abi.encodePacked(base, Strings.toString(tokenId)));
     }
 
     function balanceOf(address owner) external view override returns (uint256) {
@@ -62,6 +95,10 @@ contract ERC721 is Context, IERC721Metadata {
     }
 
     /* === INTERNAL FUNCTIONS === */
+
+    function _exists(uint256 tokenId) internal view returns (bool) {
+        return _owners[tokenId] != address(0);
+    }
 
     function _setApprovalForAll(
         address owner,
