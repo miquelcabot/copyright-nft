fa2752838b3251adc33daa65c19f81659691d646
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-06-18 10:29:32 +0200

    add inheritance of ERC721 and approveForAll functions

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -3,8 +3,9 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "./interfaces/IERC721Metadata.sol";
 
-contract ERC721 is Context {
+contract ERC721 is Context, IERC721Metadata {
     using SafeMath for uint256;
 
     string private _name;
@@ -19,15 +20,15 @@ contract ERC721 is Context {
         _symbol = symbol_;
     }
 
-    function name() external view returns (string memory) {
+    function name() external view override returns (string memory) {
         return _name;
     }
 
-    function symbol() external view returns (string memory) {
+    function symbol() external view override returns (string memory) {
         return _symbol;
     }
 
-    function balanceOf(address owner) external view returns (uint256) {
+    function balanceOf(address owner) external view override returns (uint256) {
         require(
             owner != address(0),
             "ERC721: you can't query the balance of the zero address"
@@ -35,12 +36,40 @@ contract ERC721 is Context {
         return _balances[owner];
     }
 
-    function ownerOf(uint256 tokenId) public view returns (address) {
+    function ownerOf(uint256 tokenId) public view override returns (address) {
         address owner = _owners[tokenId];
         require(
             owner != address(0),
             "ERC721: you can't query the ownership for nonexistent token"
         );
         return owner;
+    }
+
+    function setApprovalForAll(address operator, bool approved)
+        external
+        override
+    {
+        _setApprovalForAll(_msgSender(), operator, approved);
+    }
+
+    function isApprovedForAll(address owner, address operator)
+        public
+        view
+        override
+        returns (bool)
+    {
+        return _operatorApprovals[owner][operator];
+    }
+
+    /* === INTERNAL FUNCTIONS === */
+
+    function _setApprovalForAll(
+        address owner,
+        address operator,
+        bool approved
+    ) internal {
+        require(owner != operator, "ERC721: you can't approve the owner");
+        _operatorApprovals[owner][operator] = approved;
+        emit ApprovalForAll(owner, operator, approved);
     }
 }
