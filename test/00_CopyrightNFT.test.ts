36de9e9fb249f673e1cdb36d2bdea16b173d941d
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-06-18 17:15:32 +0200

    test transfer

--- test/00_CopyrightNFT.test.ts
+++ test/00_CopyrightNFT.test.ts
@@ -108,22 +108,28 @@ describe('CopyrightNFT', () => {
   /**
    * Approval for all
    */
-   describe('Approval for all', () => {
+  describe('Approval for all', () => {
     it("You can't approve the owner", async () => {
-      await expect(copyrightNFT.connect(user1).setApprovalForAll(user1.address, true)).to.be.reverted;
+      await expect(
+        copyrightNFT.connect(user1).setApprovalForAll(user1.address, true)
+      ).to.be.reverted;
     });
 
     it('Approve for all another user', async () => {
       // before approving, user2 is not approved for all user1
-      expect(await copyrightNFT.isApprovedForAll(user1.address, user2.address)).to.be.equal(false);
+      expect(
+        await copyrightNFT.isApprovedForAll(user1.address, user2.address)
+      ).to.be.equal(false);
       // approve for all user2
-      await copyrightNFT.connect(user1).setApprovalForAll(user2.address, true)
+      await copyrightNFT.connect(user1).setApprovalForAll(user2.address, true);
       // after approving, user2 is approved for all user1
-      expect(await copyrightNFT.isApprovedForAll(user1.address, user2.address)).to.be.equal(true);
+      expect(
+        await copyrightNFT.isApprovedForAll(user1.address, user2.address)
+      ).to.be.equal(true);
     });
   });
 
- /**
+  /**
    * Approve
    */
   describe('Approve', () => {
@@ -133,20 +139,77 @@ describe('CopyrightNFT', () => {
     });
 
     it("You can't approve the owner", async () => {
-      await expect(copyrightNFT.connect(user1).approve(user1.address, 1)).to.be.reverted;
+      await expect(copyrightNFT.connect(user1).approve(user1.address, 1)).to.be
+        .reverted;
     });
 
     it("You can't approve if you aren't owner nor approved for all", async () => {
-      await expect(copyrightNFT.connect(minter).approve(user1.address, 1)).to.be.reverted;
+      await expect(copyrightNFT.connect(minter).approve(user1.address, 1)).to.be
+        .reverted;
     });
 
     it('Approve another user', async () => {
       // before approving, user2 is not approved for token 1
       expect(await copyrightNFT.getApproved(1)).to.be.equal(ZERO_ADDRESS);
       // approve for all user2
-      await copyrightNFT.connect(user1).approve(user2.address, 1)
+      await copyrightNFT.connect(user1).approve(user2.address, 1);
       // after approving, user2 is approved for token 1
       expect(await copyrightNFT.getApproved(1)).to.be.equal(user2.address);
+    });
+  });
+
+  /**
+   * Transfer
+   */
+  describe('Transfer', () => {
+    beforeEach(async () => {
+      await copyrightNFT.connect(owner).setMinter(minter.address);
+      await copyrightNFT.connect(minter).mint(user1.address);
+    });
+
+    it("You can't transfer if you aren't the owner nor approved", async () => {
+      await expect(
+        copyrightNFT
+          .connect(minter)
+          .transferFrom(user1.address, user2.address, 1)
+      ).to.be.reverted;
+      await expect(
+        copyrightNFT
+          .connect(minter)
+          ['safeTransferFrom(address,address,uint256)'](
+            user1.address,
+            user2.address,
+            1
+          )
+      ).to.be.reverted;
+    });
+
+    it('Transfer a token', async () => {
+      // before transfer, we have a balance of 0
+      expect(await copyrightNFT.balanceOf(user2.address)).to.be.equal(0);
+      // transfer
+      await copyrightNFT
+        .connect(user1)
+        .transferFrom(user1.address, user2.address, 1);
+      // after transfer, we have a balance of 1
+      expect(await copyrightNFT.balanceOf(user2.address)).to.be.equal(1);
+      expect(await copyrightNFT.ownerOf(1)).to.be.equal(user2.address);
+    });
+
+    it('Safe transfer a token', async () => {
+      // before transfer, we have a balance of 0
+      expect(await copyrightNFT.balanceOf(user2.address)).to.be.equal(0);
+      // transfer
+      await copyrightNFT
+        .connect(user1)
+        ['safeTransferFrom(address,address,uint256)'](
+          user1.address,
+          user2.address,
+          1
+        );
+      // after transfer, we have a balance of 1
+      expect(await copyrightNFT.balanceOf(user2.address)).to.be.equal(1);
+      expect(await copyrightNFT.ownerOf(1)).to.be.equal(user2.address);
     });
   });
 });
