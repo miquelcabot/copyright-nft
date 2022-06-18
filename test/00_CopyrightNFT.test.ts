fc26a10f8feb5aaa1b81a04b29126ee7746caa2a
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-06-18 09:33:10 +0200

    add ownership tests

--- test/00_CopyrightNFT.test.ts
+++ test/00_CopyrightNFT.test.ts
@@ -14,9 +14,10 @@ const NFT_SYMBOL = 'MNFT';
 describe('CopyrightNFT', () => {
   let copyrightNFT: Contract;
   let owner: SignerWithAddress;
+  let minter: SignerWithAddress;
 
   beforeEach(async () => {
-    [owner] = await ethers.getSigners();
+    [owner, minter] = await ethers.getSigners();
     const CopyrightNFT = await ethers.getContractFactory('CopyrightNFT');
     copyrightNFT = await CopyrightNFT.deploy(NFT_NAME, NFT_SYMBOL);
     await copyrightNFT.deployed();
@@ -33,6 +34,23 @@ describe('CopyrightNFT', () => {
     it('Check name and symbol', async () => {
       expect(await copyrightNFT.name()).to.be.equal(NFT_NAME);
       expect(await copyrightNFT.symbol()).to.be.equal(NFT_SYMBOL);
+    });
+  });
+
+  describe('Ownership', () => {
+    it('Check owner', async () => {
+      expect(await copyrightNFT.owner()).to.be.equal(owner.address);
+    });
+
+    it('Non owner can\'t transfer ownership', async () => {
+      await expect(copyrightNFT.connect(minter).transferOwnership(minter.address)).to.be
+        .reverted;
+    });
+
+    it('Owner can transfer ownership', async () => {
+      expect(await copyrightNFT.owner()).to.be.equal(owner.address);
+      await copyrightNFT.connect(owner).transferOwnership(minter.address);
+      expect(await copyrightNFT.owner()).to.be.equal(minter.address);
     });
   });
 });
