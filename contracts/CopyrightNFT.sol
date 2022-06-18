49595299fce1f572810a470f24153b2ee11ee100
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-06-18 10:58:14 +0200

    add comments to CopyrightNFT

--- contracts/CopyrightNFT.sol
+++ contracts/CopyrightNFT.sol
@@ -5,6 +5,13 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "./ERC721.sol";
 
+/// @title  NFT that represents a copyright for a song
+/// @author Miquel A. Cabot
+/// @notice With this NFT you can demostrate the ownership of a song, and you will
+///         be able to create new ERC20 tokens to consume this song and collect
+///         profits for its use
+/// @dev    This implementation follows the EIP-721 standard
+///         (https://eips.ethereum.org/EIPS/eip-721)
 contract CopyrightNFT is Ownable, ERC721 {
     using SafeMath for uint256;
 
