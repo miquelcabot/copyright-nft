2693d88b17be333a49fad447cfb2ae88c2206ca6
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-06-18 09:17:55 +0200

    add ERC721 contract

---
+++ contracts/ERC721.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract ERC721 is Context {
+    using SafeMath for uint256;
+
+}
