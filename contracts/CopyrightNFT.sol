4db63ef1bfb691dd0bca0f9542f6de3fb71a5eb0
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-06-18 09:22:31 +0200

    add inheritance to CopyrightNFT

--- contracts/CopyrightNFT.sol
+++ contracts/CopyrightNFT.sol
@@ -1,4 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract CopyrightNFT {}
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "./ERC721.sol";
+
+contract CopyrightNFT is Ownable, ERC721 {
+    using SafeMath for uint256;
+
+    constructor(string memory name_, string memory symbol_)
+        ERC721(name_, symbol_)
+    {
+    }
+}
