88f99e4bf63b4e76376114c0343d2eefd60f011d
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-06-19 12:17:42 +0200

    add mint() to ERC20 template

--- contracts/ERC20Template.sol
+++ contracts/ERC20Template.sol
@@ -1,13 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract ERC20Template is ERC20 {
+contract ERC20Template is ERC20, Ownable {
     using SafeMath for uint256;
 
     constructor(string memory name_, string memory symbol_)
         ERC20(name_, symbol_)
     {}
+
+    function mint(address to, uint256 value) public onlyOwner {
+        _mint(to, value);
+    }
 }
